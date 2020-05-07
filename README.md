3e02c880eafc0fb8119757d4b76eaba008f13e75
Dan Büschlen <danbueschlen@gmail.com>
2020-05-07 16:30:21 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ads-scraper
+
+This tool fetches keywords from a google spreadsheet and uses them for search requests on tutti.ch and anibis.ch.
+The results will be sent via email.
+
+## Google authentication
+https://developers.google.com/sheets/api/quickstart/nodejs
+
+
+
---
+++ scrapers/tutti.js
@@ -0,0 +1,41 @@
+const https = require("https")
+const jsdom = require("jsdom")
+const {JSDOM} = jsdom
+
+const SERVICE_URL = "https://www.tutti.ch/de/li/ganze-schweiz"
+const SERIVCE_QUERY_KEY = "q"
+
+export function scrape(query) {
+  return new Promise((resolve, reject) => {
+    if (!query) {
+      reject("Please define a search query")
+      return
+    }
+
+    let url =
+      `${SERVICE_URL}?${SERIVCE_QUERY_KEY}=${encodeURIComponent(query)}`
+
+    https.get(url, res => {
+      let body = ""
+
+      res.on("data", data => body += data)
+      res.on("error", reject)
+      res.on("end", () => {
+        const dom = new JSDOM(body)
+        const ads = Array.from(dom.window.document.querySelectorAll("[data-automation='component-ad']"))
+
+        results = ads.map(ad => {
+          return {
+            title: ad.querySelector("[class^='Ad__title']").textContent,
+            price: ad.querySelector("[class^='Ad__price']").textContent,
+            location: ad.querySelector("[class^='Ad__location_info']").textContent,
+            date: ad.querySelector("[class^='Ad__date']").textContent,
+            link: "https://tutti.ch" + ad.querySelector("[class^='Ad__link']").href,
+          }
+        })
+
+        resolve(results)
+      })
+    })
+  })
+}
